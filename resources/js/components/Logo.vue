da02be2c7acb233eed88419a3be778d00c5642f0
Hakim ABDOUROIHAMANE <hakim_abdou@outlook.com>
2025-06-02 00:50:18 +0200

    wip: Prep work for the Squad page

---
+++ resources/js/components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+    <img id="logo" src="Siege_X_Guide_Logo.png" alt="Rainbow Six Siege X Operator Guide" />
+</template>
+
+<style>
+#logo {
+    max-width: 380px;
+    max-height: 110px;
+    width: auto;
+    height: auto;
+}
+@media only screen and (max-width: 760px), (min-device-width: 768px) and (max-device-width: 1024px) {
+    #logo {
+        max-width: 50vw;
+    }
+}
+</style>
